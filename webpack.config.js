214fb7d25477fad28541fb56e18e804ac77e491f
Mitch <mjoerussell@gmail.com>
2018-09-19 18:01:18 -0500

    Updated package.json and webpack config to reflect addition of node server

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+    entry: "./src/client/index.js",
+    output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        proxy: {
+            "api": "http://localhost:8080"
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['@babel/preset-react', '@babel/preset-env']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([outputDirectory]),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            favicon: "./public/favicon.ico"
+        })
+    ]
+};
