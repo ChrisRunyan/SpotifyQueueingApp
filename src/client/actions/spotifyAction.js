4f1f12f9c5b80eeba3f2dc3a8d74a905f233d94c
Mitch <mjoerussell@gmail.com>
2018-10-28 10:54:57 -0500

    Adding spotify actions and reducer

---
+++ src/client/actions/spotifyAction.js
@@ -0,0 +1,56 @@
+
+//TYPES ****
+export const PAUSE_PLAYBACK = 'PAUSE_PLAYBACK';
+export const RESUME_PLAYBACK = 'RESUME_PLAYBACK';
+export const REFRESH_AUTHORIZATION = 'REFRESH_AUTHORIZATION';
+export const REQUEST_PENDING = 'REQUEST_PENDING';
+// ****
+
+//ENDPOINTS ****
+const BASE_ENDPOINT = 'https://api.spotify.com/v1/';
+const SEARCH_ENDPOINT = BASE_ENDPOINT + 'search';
+const PLAYER_ENDPOINT = BASE_ENDPOINT + 'me/player';
+// ****
+
+const makeRequest = (access_token, endpoint, data) => {
+    return new Request(endpoint, {
+        headers: {
+            'Authorization': 'Bearer ' + access_token,
+            'Accept': 'application/json',
+            'content-type': 'application/json',
+        },
+        method: 'PUT',
+        data: data,
+    });
+}
+
+const handleError = err => {
+    if(err.status === 401) { /* ACCESS TOKEN EXPIRED */ }
+    /* START HANDLING ERRORS DIFFERENTLY HERE */
+}
+
+export const pausePlayback = access_token => dispatch => {
+    const request = makeRequest(access_token, PLAYER_ENDPOINT + '/pause', null)
+    dispatch({ type: REQUEST_PENDING })
+    fetch(request)
+    .then(res => res.json())
+    .then(response => {
+        dispatch({
+            type: PAUSE_PLAYBACK,
+        })
+    })
+    .catch(error => handleError(error))
+}
+
+export const pausePlayback = access_token => dispatch => {
+    const request = makeRequest(access_token, PLAYER_ENDPOINT + '/play', null)
+    dispatch({ type: REQUEST_PENDING })
+    fetch(request)
+    .then(res => res.json())
+    .then(response => {
+        dispatch({
+            type: RESUME_PLAYBACK,
+        })
+    })
+    .catch(error => handleError(error))
+}
---
+++ src/client/reducers/spotifyReducer.js
@@ -0,0 +1,16 @@
+import { PAUSE_PLAYBACK, RESUME_PLAYBACK } from '../actions/spotifyAction'
+
+export default (state = {}, action) => {
+    switch(action.type) {
+        case PAUSE_PLAYBACK: 
+            return {
+                ...state,
+                isPlaying: false
+            }
+        case RESUME_PLAYBACK:
+            return {
+                ...state,
+                isPlaying: true
+            }
+    }
+}
