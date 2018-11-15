608e312cfcf106c83f931849634a13090a5e79e9
Mitch <mjoerussell@gmail.com>
2018-11-14 22:43:11 -0600

    Added classes to firebase client, removed API keys from firebase server

--- src/client/App.js
+++ src/client/App.js
@@ -4,23 +4,10 @@ import './styles/App.css';
 import { Table, Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
 
-<<<<<<< HEAD
-import io from 'socket.io-client'
-
-const socket = io({ path: '/ws' })
-
-class App extends Component {
-
-  getHashParams = () => {
-    let hashParams = {};
-    let e, r = /([^&;=]+)=?([^&;]*)/g,
-    q = window.location.hash.substring(1);
-=======
 // import PropTypes from 'prop-types'
 // import { connect } from 'react-redux'
 // import { loginSuccess } from './actions/loginAction'
 // import { joinRoom, fetchSong, pushSong } from './actions/firebaseAction'
->>>>>>> b6dd6591896b741b64ca9bddd8cddb84455c318d
 
 import Song from './components/Song'
 import SongSearch from './components/SongSearch'
@@ -28,75 +15,10 @@ import SongSearch from './components/SongSearch'
 // import SongListItem from './components/SongListItem'
 // import { getCurrentPlaybackState } from './actions/spotifyAction';
 
-<<<<<<< HEAD
-    return hashParams
-  }
+import io from 'socket.io-client'
+import { FirebaseWrapper } from './firebase'
+const socket = io({ path: '/ws' })
 
-  componentWillMount() {
-    const hashParams = this.getHashParams()
-    if (!hashParams.access_token) {
-      window.location.href = '/api/login'
-      //socket.emit('login')
-    } else {
-      socket.emit('login')
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.auth.access_token) {
-      this.props.readPlayback({ access_token: nextProps.auth.access_token })
-    }
-  }
-
-  componentDidMount() {
-    console.log("DidMount: Going to emit")
-    socket.emit("ping")
-    socket.emit('firebase-join', "test_room")
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Apollo Queue for Spotify</h1>
-        </header>
-        <button onClick={() => socket.emit("firebase-join", "test_room")} >Join</button>
-        {/* <SongListItem>
-          <SongControls />
-        </SongListItem> */}
-      </div>
-    );
-  }
-}
-
-App.propTypes = {
-  auth: PropTypes.objectOf(PropTypes.string),
-  room: PropTypes.objectOf(PropTypes.string),
-  songs: PropTypes.objectOf(PropTypes.string),
-  login: PropTypes.func,
-  joinRoom: PropTypes.func,
-  addSong: PropTypes.func,
-  readPlayback: PropTypes.func,
-}
-
-const mapStateToProps = state => {
-  return {
-    auth: state.login,
-    room: state.firebase.room,
-    songs: state.firebase.songs,
-    isPlaying: state.spotify.isPlaying,
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    login: (authParams = null) => { dispatch(loginSuccess(authParams)) },
-    joinRoom: (roomId) => { dispatch(joinRoom(roomId)) },
-    getSong: (songId) => { dispatch(fetchSong(songId)) },
-    addSong: (roomId, songId) => { dispatch(pushSong(roomId, songId)) },
-    readPlayback: tokens => dispatch(getCurrentPlaybackState(tokens)),
-=======
 class App extends Component {
   state = {
     roomCode: "HJRA",
@@ -127,6 +49,11 @@ class App extends Component {
       },
     ]
   };
+
+  componentDidMount() {
+    const wrapper = new FirebaseWrapper(socket)
+    wrapper.joinRoom("test_room_id")
+  }
 
   render() {
     return(
@@ -176,7 +103,6 @@ class App extends Component {
       </Grid>
       
     )
->>>>>>> b6dd6591896b741b64ca9bddd8cddb84455c318d
   }
 }
 
--- src/client/firebase.js
+++ src/client/firebase.js
@@ -1,20 +1,64 @@
-import firebase from 'firebase'
-
-const config = {
-    apiKey: "AIzaSyDqOLOBFwnUPqRAVRZY2HjDKNAZ3ZqlzKU",
-    authDomain: "apollo-queue-app.firebaseapp.com",
-    databaseURL: "https://apollo-queue-app.firebaseio.com",
-    projectId: "apollo-queue-app",
-    storageBucket: "apollo-queue-app.appspot.com",
-    messagingSenderId: "873661626171"
+class SpotifyData {
+  constructor(id, name, query) {
+    this.name = name ? name : ''
+    this.id = id ? id : ''
+    this.query = query ? query : ''
   }
 
-const app = firebase.initializeApp(config)
+  get name() { return this.name }
+  get id() { return this.id }
+  get query() { return this.query }
 
-export default app
-export const databaseRef = app.database().ref()
-export const roomRef = databaseRef.child("rooms");
-export const songRef = databaseRef.child("songs");
+}
 
-//TODO get rooms ref && export
-//TODO get songs ref && export
+export class Album extends SpotifyData{
+  constructor(id, name, query) {
+    super(id, name, query)
+  }
+}
+
+export class Artist extends SpotifyData{
+  constructor(id, name, query) {
+    super(id, name, query)
+  }
+}
+
+export class Song extends SpotifyData {
+  constructor(song) {
+    super(song.id, song.name, song.query)
+
+    this.addedBy = song.addedBy ? song.addedBy : ''
+    this.votes = song.votes ? song.votes : 0
+
+    this.album = new Album(
+      song.album.id, song.album.name, song.album.query)
+    this.artist = new Artist(
+      song.artist.id, song.artist.name, song.artist.query)
+  }
+
+}
+
+export class Room {
+  constructor(room) {
+    this.roomOwner = room.roomOwner;
+    this.roomCode = room.roomCode;
+    this.roomName = room.roomName;
+    this.access_token = room.access_token;
+    this.songs = room.songs.map(s => new Song(s))
+  }
+
+}
+
+export class FirebaseWrapper {
+  constructor(socket) {
+    this.socket = socket
+  }
+
+  joinRoom = roomId => {
+    fetch(`/api/fire/join/${roomId}`)
+    // .then(
+
+    // )
+  }
+
+}
