f80ec69fe3d6d06621c22652aafcf0f0ff0a23cf
Mitch <mjoerussell@gmail.com>
2018-12-03 09:30:00 -0600

    Clicking play will now play top-voted song.  Timing appears to work properly

--- src/client/components/SongControls.js
+++ src/client/components/SongControls.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Image, Navbar, NavItem, Glyphicon, ProgressBar } from 'react-bootstrap';
+import {
+	Image,
+	Navbar,
+	NavItem,
+	Glyphicon,
+	ProgressBar,
+} from 'react-bootstrap';
 import { TimeoutInterval, Timer } from '../classes/Timer';
 
 const progressInterval = 500;
-const lockInPercent = .95;
+const lockInPercent = 0.95;
 
 class SongControls extends React.Component {
 	constructor(props) {
@@ -13,9 +19,9 @@ class SongControls extends React.Component {
 			isPlaying: false,
 			progress: 0,
 			duration: 0,
-            playbackTimer: null,
-            lockInTimer: null,
-			albumArt: ''
+			playbackTimer: null,
+			lockInTimer: null,
+			albumArt: '',
 		};
 	}
 
@@ -36,7 +42,7 @@ class SongControls extends React.Component {
 	// 						console.log(`progress: ${this.state.progress}`);
 	// 						console.log(`art: ${this.state.albumArt}`);
 	// 						this.setState({
-								
+
 	// 							progress:
 	// 								this.state.progress + progressInterval,
 	// 						});
@@ -44,7 +50,7 @@ class SongControls extends React.Component {
 	// 					progressInterval,
 	// 					res.item.duration_ms - res.progress_ms
 	// 				),
-					
+
 	// 			},
 	// 			() => {
 	// 				if (this.state.isPlaying) {
@@ -61,57 +67,106 @@ class SongControls extends React.Component {
 	togglePlayback = () => {
 		if (this.state.isPlaying) {
 			this.props.spotify.pause();
-			this.state.timer.pause();
+			this.pauseTimers();
 		} else {
-            if (!this.state.hasSong) {
-                const nextSong = this.getNextSong(this.props.songs);
-                const secondSong = this.getNextSong(this.props.songs, 1);
-                this.props.spotify.playNextFromPlaylist(this.props.playlistId, currentSong.data.id);
-                
-
-            } else {
-                this.props.spotify.play();
-                this.state.timer.resume();
-            }
-            
+			if (!this.state.hasSong) {
+				const nextSong = this.getNextSong(this.props.songs);
+				const secondSong = this.getNextSong(this.props.songs, 1);
+				this.props.spotify.playNextFromPlaylist(
+					this.props.playlistId,
+					nextSong.data.id
+				);
+				setTimeout(() => {
+					this.props.spotify.getPlayerInfo(res => {
+						console.log(`get info: isPlaying=${res.is_playing}`);
+						this.setState(
+							{
+								isPlaying: res.is_playing,
+								progress: res.progress_ms,
+								duration: res.item.duration_ms,
+								albumArt: res.item.album.images[2].url,
+							},
+							() => {
+								if (this.state.isPlaying) {
+									console.log('start timers');
+									this.startPlaybackTimers(
+										nextSong,
+										secondSong
+									);
+									this.setState({
+										hasSong: true,
+									});
+								}
+							}
+						);
+					});
+				}, 500);
+			} else {
+				this.props.spotify.play();
+				this.resumeTimers();
+			}
 		}
 		this.setState({
 			isPlaying: !this.state.isPlaying,
 		});
-    };
+	};
 
-    startPlaybackTimers = (currentSong, nextSong) => {
-        const timeOfLockIn = (this.state.duration * lockInPercent) - this.state.progress;
-        const playbackTimer = new TimeoutInterval(
-            () => {
-                this.setState({
-                    progress: this.state.progress + progressInterval
-                });
-            }, 
-            progressInterval,
-            this.state.duration - this.state.progress,
-            () => {
-                this.props.removeSong(currentSong.key);
+	startPlaybackTimers = (currentSong, nextSong) => {
+		const timeOfLockIn =
+			this.state.duration * lockInPercent - this.state.progress;
+		let lockInTimer = null;
+		const playbackTimer = new TimeoutInterval(
+			() => {
+				console.log(`progress: ${this.state.progress} ms`);
+				this.setState({
+					progress: this.state.progress + progressInterval,
+				});
+			},
+			progressInterval,
+			this.state.duration - this.state.progress,
+			() => {
+				this.props.removeSong(currentSong.key);
+				this.setState({ hasSong: false });
+			}
+		);
+		if (nextSong) {
+			lockInTimer = new Timer(() => {
+				this.props.lockSong(nextSong.key);
+				this.reorderSongsInPlaylist(currentSong, nextSong);
+			}, timeOfLockIn);
+		}
+		this.setState(
+			{
+				playbackTimer,
+				lockInTimer,
+			},
+			() => {
+				this.resumeTimers();
+			}
+		);
+	};
 
-            }
-        );
-        if(nextSong) {
-            const lockInTimer = new Timer(
-                () => {
-                    this.props.lockSong(nextSong.key);
-                    this.reorderSongsInPlaylist(currentSong, nextSong);
-                }, timeOfLockIn);
-        }
-        this.setState({
-            playbackTimer,
-            lockInTimer
-        }, () => {
-            playbackTimer.resume();
-            lockInTimer || lockInTimer.resume();    // only resume lockInTimer if it exists
-        });
-    }
+	pauseTimers = () => {
+		console.log('pause');
+		if (this.state.playbackTimer) {
+			this.state.playbackTimer.pause();
+		}
+		if (this.state.lockInTimer) {
+			this.state.lockInTimer.pause();
+		}
+	};
 
-    reorderSongsInPlaylist = (currentSong, nextSong) => {
+	resumeTimers = () => {
+		console.log('resume');
+		if (this.state.playbackTimer) {
+			this.state.playbackTimer.resume();
+		}
+		if (this.state.lockInTimer) {
+			this.state.lockInTimer.resume();
+		}
+	};
+
+	reorderSongsInPlaylist = (currentSong, nextSong) => {
 		const playlistId = this.props.playlistId;
 		const getIndex = this.props.spotify.getIndexOfSongInPlaylist;
 		getIndex(
@@ -139,17 +194,17 @@ class SongControls extends React.Component {
 		);
 	};
 
-    getNextSong = (songs, offset = 0) => {
-        if(songs) {
-            songs.sort((a, b) => {
-                b.data.votes - a.data.votes;
-            })
-            if (songs[offset]) {
-                return songs[offset];
-            }
-        }
-        return null;
-    }
+	getNextSong = (songs, offset = 0) => {
+		if (songs) {
+			songs.sort((a, b) => {
+				b.data.votes - a.data.votes;
+			});
+			if (songs[offset]) {
+				return songs[offset];
+			}
+		}
+		return null;
+	};
 
 	render() {
 		const playIcon = (
@@ -164,7 +219,7 @@ class SongControls extends React.Component {
 		);
 		return (
 			<Navbar inverse fixedBottom>
-				<Image src= {this.state.albumArt}/>
+				<Image src={this.state.albumArt} />
 				<Navbar.Brand>Controls</Navbar.Brand>
 				<NavItem onClick={this.togglePlayback}>
 					{this.state.isPlaying ? pauseIcon : playIcon}
