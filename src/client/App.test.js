7f1d9edca6d845d01b2a8726ccf7ee6ebaee1c86
Mitch <mjoerussell@gmail.com>
2018-12-05 08:26:07 -0600

    Fixed node-modules

--- src/client/App.test.js
+++ src/client/App.test.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import Room from './Room';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Room />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+
+	const songs = [
+		{
+			key: 'key',
+			data: {
+				votes: 0,
+			},
+		},
+	];
+	const room = {
+		access_token: 'ddd',
+		refresh_token: 'rrr',
+		songs: songs,
+	};
+	ReactDOM.render(<Room room={room} />, div);
+	ReactDOM.unmountComponentAtNode(div);
 });
